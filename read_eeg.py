a6e5e67beb51b0bfe7dd2f1710bae1e9390d5657
Ana <ana@MacBook-Pro-de-Ana.local>
2018-02-03 12:53:41 +0100

    No me funciona biosppy

--- read_eeg.py
+++ read_eeg.py
@@ -10,6 +10,8 @@ Created on Tue Jan 30 19:57:05 2018
 
 import mne
 import matplotlib.pyplot as plt
+import biosppy.signals.eeg as eeg
+import numpy as np
 
 raw_eeg = mne.io.read_raw_edf('./data/S001R01.edf',preload = True, verbose=False)
 
@@ -24,5 +26,9 @@ fs = raw_eeg.info['sfreq']
 
 plt.plot(time,eeg[:3,:].T)
 
-
 #EEG numpy array
+
+signal = np.array(eeg)
+
+#EEG Features
+ts,filtro, features_ts,theta,alpha_low,alpha_high,beta,gamma,plf_pairs,plf = eeg(signal = signal, sampling_rate=fs) 
